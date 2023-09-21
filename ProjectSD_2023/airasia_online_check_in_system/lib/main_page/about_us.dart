a4111b2ef94c844fcef8410e11dd3a953e48e198
fieanfiean <ooichristopher404@gmail.com>
2023-09-22 00:12:14 +0800

    View & Edit About Us and Contact Us(Admin & Staff)

--- ProjectSD_2023/airasia_online_check_in_system/lib/main_page/about_us.dart
+++ ProjectSD_2023/airasia_online_check_in_system/lib/main_page/about_us.dart
@@ -1,12 +1,41 @@
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
 
-class AboutUsPage extends StatelessWidget {
+class AboutUsPage extends StatefulWidget {
   const AboutUsPage({super.key});
+
+  @override
+  State<AboutUsPage> createState() => _AboutUsPageState();
+}
+
+class _AboutUsPageState extends State<AboutUsPage> {
+  final dbRef = FirebaseDatabase.instance.ref('aboutUs');
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('About Us')),
+      appBar: AppBar(title:const Text('About Us')),
+      body: Column(
+        children: [
+          Expanded(
+            child: FirebaseAnimatedList(
+              query: dbRef,
+              itemBuilder:(context, snapshot, animation, index){
+                return ListTile(
+                  title: Text(snapshot.child('title').value.toString()),
+                  subtitle: Text(snapshot.child('content').value.toString()),
+                );
+              }
+            )
+          )
+        ],
+      )
     );
   }
 }
--- ProjectSD_2023/airasia_online_check_in_system/lib/main_page/contact_us.dart
+++ ProjectSD_2023/airasia_online_check_in_system/lib/main_page/contact_us.dart
@@ -1,12 +1,44 @@
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
 
-class ContactUsPage extends StatelessWidget {
-  const ContactUsPage({super.key});
+
+class ContactUsPage extends StatefulWidget {
+  const ContactUsPage({Key? key}) : super(key: key);
+
+  @override
+  _ContactUsPageState createState() => _ContactUsPageState();
+}
+
+class _ContactUsPageState extends State<ContactUsPage> { 
+  final dbRef = FirebaseDatabase.instance.ref('contactUs');
+
+  @override
+  void initState() {
+    super.initState();
+  }
+  
+  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Contact Us')),
+      appBar: AppBar(title:const Text('Contact Us')),
+      body: Column(
+        children: [
+          Expanded(
+            child: FirebaseAnimatedList(
+              query: dbRef,
+              itemBuilder:(context, snapshot, animation, index){
+                return ListTile(
+                  title: Text(snapshot.child('title').value.toString()),
+                  subtitle: Text(snapshot.child('content').value.toString()),
+                );
+              }
+            )
+          )
+        ],
+      )
     );
   }
 }
